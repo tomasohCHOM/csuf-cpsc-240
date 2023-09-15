faf79ccce44958a913733928d6dbdc5b6c41dea4
Tomas Oh <tomasoh@csu.fullerton.edu>
2023-09-15 13:53:39 -0700

    Updated run.sh accordingly

--- assignment 02/run.sh
+++ assignment 02/run.sh
@@ -13,7 +13,7 @@ echo "Compile the C module main-driver.c"
 gcc -c -m64 -Wall -o main-driver.o main-driver.c -fno-pie -no-pie -std=c17
 
 echo "Assemble the module manage.asm"
-nasm -f elf64 -l main.lis -o manage.o manage.asm
+nasm -f elf64 -l manage.lis -o manage.o manage.asm
 
 echo "Assemble the module input_array.asm"
 nasm -f elf64 -l input_array.lis -o input_array.o input_array.asm
@@ -21,10 +21,10 @@ nasm -f elf64 -l input_array.lis -o input_array.o input_array.asm
 echo "Assemble the module output_array.asm"
 nasm -f elf64 -l output_array.lis -o output_array.o output_array.asm
 
-echo "Link the two object files already created"
-gcc -m64 -o main.out main-driver.o manage.o -fno-pie -no-pie -std=c17
+echo "Link all the object files already created"
+gcc -m64 -o arr.out main-driver.o manage.o input_array.o output_array.o -fno-pie -no-pie -std=c17
 
 echo "Run the program Assignment 2 - Array Management System"
-./main.out
+./arr.out
 
 echo "The bash script file is now closing."
