86c758bfe152ec3ede11884f4145aca69c4b3044
Tomas Oh <tomasoh@csu.fullerton.edu>
2023-09-15 11:29:56 -0700

    Progressing with input_array

--- assignment 02/input_array.asm
+++ assignment 02/input_array.asm
@@ -1,3 +1,55 @@
 global input_array
 
+segment .data:
+    floatform db "%lf", 0
+
 segment .text:
+
+input_array:
+    ; Back up all the GPRs
+    push    rbp
+    mov     rbp, rsp
+    push    rbx
+    push    rcx
+    push    rdx
+    push    rsi
+    push    rdi
+    push    r8
+    push    r9
+    push    r10
+    push    r11
+    push    r12
+    push    r13
+    push    r14
+    push    r15
+    pushf
+
+    mov     r14, rdi    ; r14 is the array
+    mov     r15, rsi    ; r15 is the upper-limit of the number of cells in the array
+    mov     r13, 0      ; r13 to count input
+
+    push qword   0
+    mov     rax, 0
+    mov     rdi, floatform
+    mov     rdi, rsp
+    call    scanf       ; either a float number or ctrl-d
+
+
+    ; Restoring the original value to the GPRs
+    popf
+    pop     r15
+    pop     r14
+    pop     r13
+    pop     r12
+    pop     r11
+    pop     r10
+    pop     r9
+    pop     r8
+    pop     rdi
+    pop     rsi
+    pop     rdx
+    pop     rcx
+    pop     rbx
+    pop     rbp
+
+    ret
--- assignment 02/output_array.asm
+++ assignment 02/output_array.asm
@@ -16,24 +16,23 @@ segment .data
 segment .text
 
 show_numbers:
-
-    ;Back up the general purpose registers for the sole purpose of protecting the data of the caller.
-    push rbp                                          ;Backup rbp
-    mov  rbp,rsp                                      ;The base pointer now points to top of stack
-    push rdi                                          ;Backup rdi
-    push rsi                                          ;Backup rsi
-    push rdx                                          ;Backup rdx
-    push rcx                                          ;Backup rcx
-    push r8                                           ;Backup r8
-    push r9                                           ;Backup r9
-    push r10                                          ;Backup r10
-    push r11                                          ;Backup r11
-    push r12                                          ;Backup r12
-    push r13                                          ;Backup r13
-    push r14                                          ;Backup r14
-    push r15                                          ;Backup r15
-    push rbx                                          ;Backup rbx
-    pushf                                             ;Backup rflags
+    ; Back up all the GPRs
+    push    rbp
+    mov     rbp, rsp
+    push    rbx
+    push    rcx
+    push    rdx
+    push    rsi
+    push    rdi
+    push    r8
+    push    r9
+    push    r10
+    push    r11
+    push    r12
+    push    r13
+    push    r14
+    push    r15
+    pushf
 
     ;Registers rax, rip, and rsp are usually not backed up.
 
@@ -69,21 +68,21 @@ done:
     ;return 0 which is the traditional signal of success
     xor rax,rax
 
-    ;Restore all the previously pushed registers.
-    popf                                    ;Restore rflags
-    pop rbx                                 ;Restore rbx
-    pop r15                                 ;Restore r15
-    pop r14                                 ;Restore r14
-    pop r13                                 ;Restore r13
-    pop r12                                 ;Restore r12
-    pop r11                                 ;Restore r11
-    pop r10                                 ;Restore r10
-    pop r9                                  ;Restore r9
-    pop r8                                  ;Restore r8
-    pop rcx                                 ;Restore rcx
-    pop rdx                                 ;Restore rdx
-    pop rsi                                 ;Restore rsi
-    pop rdi                                 ;Restore rdi
-    pop rbp                                 ;Restore rbp
+    ; Restoring the original value to the GPRs
+    popf
+    pop        r15
+    pop        r14
+    pop        r13
+    pop        r12
+    pop        r11
+    pop        r10
+    pop        r9
+    pop        r8
+    pop        rdi
+    pop        rsi
+    pop        rdx
+    pop        rcx
+    pop        rbx
+    pop        rbp
 
-    ret                                     ;Pop the integer stack and jump to the address equal to the popped value.
+    ret
