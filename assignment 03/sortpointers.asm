eb37c0380dd81162fc142ab0eb93a7773c953c5d
Tomas Oh <tomasoh@csu.fullerton.edu>
2023-10-05 19:40:50 -0700

    Started sortpointers in assembly

--- assignment 03/sortpointers.asm
+++ assignment 03/sortpointers.asm
@@ -26,3 +26,76 @@
 ; Purpose of this file:
 ;    This is the C driver that contains the start code and which calls the
 ;    assembly function director from director.asm.
+
+global sort_pointers
+
+; Used for state component backup/restore
+segment .bss
+    align   64
+    backup  resb 832
+
+segment .text
+
+input_array:
+    ; Back up all the GPRs
+    push    rbp
+    mov     rbp, rsp
+    push    rbx
+    push    rcx
+    push    rdx
+    push    rsi
+    push    rdi
+    push    r8
+    push    r9
+    push    r10
+    push    r11
+    push    r12
+    push    r13
+    push    r14
+    push    r15
+    pushf
+
+    ;==== Perform State Component Backup ====
+    mov         rax, 7
+    mov         rdx, 0
+    xsave       [backup]
+    ;==== End State Component Backup ========
+
+    mov         r14, rdi    ; r14 is the array
+    mov         r15, rsi    ; r15 is the upper-limit of the number of cells in the array
+    xor         r13, r13    ; r13 is the outer counter
+    xor         r12, r12    ; r12 is the inner counter
+    jmp         outerloop
+
+outerloop:
+    
+
+done:
+    ;==== Perform State Component Restore ====
+    mov     rax, 7
+    mov     rdx, 0
+    xrstor  [backup]
+    ;==== End State Component Restore ========
+
+    ; r13 holds the count of numbers in the array.
+    ; Move it to rax as we are required to return that number.
+    mov     rax, r13
+
+    ; Restoring the original value to the GPRs
+    popf
+    pop     r15
+    pop     r14
+    pop     r13
+    pop     r12
+    pop     r11
+    pop     r10
+    pop     r9
+    pop     r8
+    pop     rdi
+    pop     rsi
+    pop     rdx
+    pop     rcx
+    pop     rbx
+    pop     rbp
+
+    ret
