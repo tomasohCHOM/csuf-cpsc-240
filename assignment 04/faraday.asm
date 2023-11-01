11a895c9cef8cd35ff900649b35905c9c9c8d3e2
Tomas Oh <tomasoh@csu.fullerton.edu>
2023-11-01 11:42:18 -0700

    Upgraded comments, created setup for using isfloat for floating-point number validation

--- assignment 04/faraday.asm
+++ assignment 04/faraday.asm
@@ -22,10 +22,8 @@ segment .data
     calculation_message         db "Thank you %s. We at Majestic are pleased to inform you that your system performed %lf joules of work.", 10, 10, 0
     congratulations_message     db "Congratulations %s. Come back any time and make use of our software.", 10, 0
     final_message               db "Everyone with title %s is welcome to use our programs at a reduced price.", 10, 10, 0
-    
-    negative_message    db  "You inputted a negative or 0 as a number.", 10, 0
-    greater_num_message db  "You inputted a number greater than or equal to the number of miles.", 10, 0
-    invalid_message     db  "The input is invalid and was rejected by the program. The program will soon terminate. Please run it again.", 10, 0
+
+    invalid_message     db  "Attention %s. Invalid inputs have been encountered. Please run the program again.", 10, 0
 
     two_hundred_fifty_three_point_five  dq 253.5
     two_point_zero                      dq 2.0
@@ -36,29 +34,30 @@ segment .data
 segment .bss
     align   64
     backup  resb 832
-    name    resb 32
+    name    resb 40
     title   resb 40
+    num     resb 40
 
 ; Begin executable instructions
 segment .text
 
 faraday:
     ; Back up all the GPRs
-    push    rbp
-    mov     rbp, rsp
-    push    rbx
-    push    rcx
-    push    rdx
-    push    rsi
-    push    rdi
-    push    r8
-    push    r9
-    push    r10
-    push    r11
-    push    r12
-    push    r13
-    push    r14
-    push    r15
+    push        rbp
+    mov         rbp, rsp
+    push        rbx
+    push        rcx
+    push        rdx
+    push        rsi
+    push        rdi
+    push        r8
+    push        r9
+    push        r10
+    push        r11
+    push        r12
+    push        r13
+    push        r14
+    push        r15
     pushf
 
     ;==== Perform State Component Backup ====
@@ -73,14 +72,15 @@ faraday:
     mov         rsi, name_prompt_message
     call        printf
 
-    ; Receive input (representing the user's name) through
-    ; using fgets, trimming the newline with strlen
+    ; Receive input (representing the user's name) with fgets
     mov         rax, 0
     mov         rdi, name
-    mov         rsi, 32
+    mov         rsi, 40
     mov         rdx, [stdin]
     call        fgets
 
+    ; Strip the newline character by getting the string's length
+    ; and removing the last character of the string
     mov         rax, 0
     mov         rdi, name
     call        strlen
@@ -93,108 +93,130 @@ faraday:
     mov         rsi, title_prompt_message
     call        printf
 
-    ; Receive input (representing the user's title) through
-    ; using fgets, trimming the newline with strlen
+    ; Receive input (representing the user's title) with fgets
     mov         rax, 0
     mov         rdi, title
     mov         rsi, 40
     mov         rdx, [stdin]
     call        fgets
 
+    ; Strip the newline character by getting the string's length
+    ; and removing the last character of the string
     mov         rax, 0
     mov         rdi, title
     call        strlen
 
     mov         byte [title + rax - 1], 0
 
-    ; Welcome the user given their title and their title
+    ; Welcome the user given their title
     mov         rax, 0
-    mov         rdi, string_format
-    mov         rsi, welcome_message
+    mov         rdi, welcome_message
+    mov         rsi, title
     call        printf
 
-    ; Prompt for the speed of the intial segment of the trip
+    ; Prompt for the voltage of the electrical system
     mov qword   rax, 0
     mov         rdi, string_format
     mov         rsi, voltage_prompt_message
     call        printf
 
-    ; Obtain the initial speed from standard input
-    mov qword   rax, 0
-    mov         rdi, eight_byte_format
-    mov         rsi, rsp
-    call        scanf
-    movsd       xmm8, qword [rsp]
+    ; mov qword   rax, 0
+    ; mov         rdi, eight_byte_format
+    ; mov         rsi, rsp
+    ; call        scanf
+    ; movsd       xmm8, qword [rsp]
+    mov         rax, 0
+    mov         rdi, num
+    mov         rsi, 40
+    mov         rdx, [stdin]
+    call        fgets
 
-    ; Prompt for the number of miles that the speed will be maintained
+    ; Strip the newline character by getting the string's length
+    ; and removing the last character of the string
+    mov         rax, 0
+    mov         rdi, title
+    call        strlen
+
+    mov         byte [title + rax - 1], 0
+
+    ; Validate that the input is a floating-point number
+    mov         rax, 0
+    mov         rdi, num
+    call        isfloat
+    cmp         rax, 0
+    je          invalid
+
+    mov         rax, 0
+    mov         rdi, num
+    call        atof
+    movsd       xmm8, xmm0
+
+    ; Prompt for the electrical resistance in the system
     mov qword   rax, 0
     mov         rdi, string_format
     mov         rsi, resistance_prompt_message
     call        printf
 
-    ; Obtain the number of miles that this speed will be maintained
     mov qword   rax, 0
     mov         rdi, eight_byte_format
     mov         rsi, rsp
     call        scanf
     movsd       xmm9, qword [rsp]
 
-    ; Prompt for the speed of the final segment of the trip
+    ; Prompt for the time that the system has been operating for
     mov qword   rax, 0
     mov         rdi, string_format
     mov         rsi, time_prompt_message
     call        printf
 
-    ; Obtain the final speed from standard input
     mov qword   rax, 0
     mov         rdi, eight_byte_format
     mov         rsi, rsp
     call        scanf
     movsd       xmm10, qword [rsp]
 
+    ; xmm8 = V, xmm9 = R, xmm10 = T
+    ; xmm11 = V / R = I
     movsd       xmm11, xmm8
     divsd       xmm11, xmm9
+
+    ; xmm12 = V x I = P
     movsd       xmm12, xmm8
     mulsd       xmm12, xmm11
+
+    ; xmm13 = P x T = W
     movsd       xmm13, xmm12
     mulsd       xmm12, xmm10
 
+    ; Print the calculated result to the user
     mov         rax, 1
     mov         rdi, calculation_message
+    mov         rsi, title
     movsd       xmm0, xmm12
     call        printf
 
+    ; Congratulate the user before returning to the C driver
     mov         rax, 0
     mov         rdi, congratulations_message
+    mov         rsi, name
     call        printf
 
     mov         rax, 0
     mov         rdi, final_message
+    mov         rsi, title
     call        printf
 
-    jmp         setreturnvalue
+    jmp         done
 
-negative:
-    mov         rax, 0
-    mov         rdi, negative_message
-    call        printf
-
+invalid:
     mov         rax, 0
     mov         rdi, invalid_message
+    mov         rsi, title
     call        printf
-    jmp         setreturnvalue
+    
+    jmp         done
 
-greater_than_or_equal_to_miles:
-    mov         rax, 0
-    mov         rdi, greater_num_message
-    call        printf
-
-    mov         rax, 0
-    mov         rdi, invalid_message
-    call        printf
-    jmp         setreturnvalue
-
-setreturnvalue:
+done:
     sub         rsp, 8
     movsd       qword [rsp], xmm12
 
@@ -210,20 +232,20 @@ setreturnvalue:
     movsd       xmm0, xmm12
     ; Restoring the original value to the GPRs
     popf
-    pop        r15
-    pop        r14
-    pop        r13
-    pop        r12
-    pop        r11
-    pop        r10
-    pop        r9
-    pop        r8
-    pop        rdi
-    pop        rsi
-    pop        rdx
-    pop        rcx
-    pop        rbx
-    pop        rbp
+    pop         r15
+    pop         r14
+    pop         r13
+    pop         r12
+    pop         r11
+    pop         r10
+    pop         r9
+    pop         r8
+    pop         rdi
+    pop         rsi
+    pop         rdx
+    pop         rcx
+    pop         rbx
+    pop         rbp
 
     ret
 
--- assignment 04/r.sh
+++ assignment 04/r.sh
@@ -12,11 +12,14 @@ echo "This is program <Assignment 4 - How to use strings>"
 echo "Assemble the module faraday.asm"
 nasm -f elf64 -l faraday.lis -o faraday.o faraday.asm
 
+echo "Assemble the module isfloat.asm"
+nasm -f elf64 -l isfloat.lis -o isfloat.o isfloat.asm
+
 echo "Compile the C module ampere.c"
 gcc -c -m64 -Wall -o ampere.o ampere.c -fno-pie -no-pie -std=c17
 
 echo "Link the two object files already created"
-gcc -m64 -o program.out ampere.o faraday.o -fno-pie -no-pie -std=c17
+gcc -m64 -o program.out ampere.o faraday.o isfloat.o -fno-pie -no-pie -std=c17
 
 echo "Run the program Assignment 4 - How to use strings"
 ./program.out
